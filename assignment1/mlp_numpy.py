b20ad68ae48eb6a5e64ff5aa8490ceac8c79cf59
NeilTime <LeonTyrus@gmail.com>
2023-11-17 20:14:03 +0100

    first commit

---
+++ assignment1/mlp_numpy.py
@@ -0,0 +1,135 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2023 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2023
+# Date Created: 2023-11-01
+################################################################################
+"""
+This module implements a multi-layer perceptron (MLP) in NumPy.
+You should fill in code into indicated sections.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from modules import *
+
+
+class MLP(object):
+    """
+    This class implements a Multi-layer Perceptron in NumPy.
+    It handles the different layers and parameters of the model.
+    Once initialized an MLP object can perform forward and backward.
+    """
+
+    def __init__(self, n_inputs, n_hidden, n_classes):
+        """
+        Initializes MLP object.
+
+        Args:
+          n_inputs: number of inputs.
+          n_hidden: list of ints, specifies the number of units
+                    in each linear layer. If the list is empty, the MLP
+                    will not have any linear layers, and the model
+                    will simply perform a multinomial logistic regression.
+          n_classes: number of classes of the classification problem.
+                     This number is required in order to specify the
+                     output dimensions of the MLP
+
+        TODO:
+        Implement initialization of the network.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        self.layers = []
+        layer_sizes = [n_inputs] + n_hidden + [n_classes]
+
+        for i in range(len(layer_sizes) - 1):
+            self.layers.append(LinearModule(layer_sizes[i], layer_sizes[i+1], input_layer=(i==0)))
+            if i < len(n_hidden):
+                self.layers.append(ELUModule())
+
+        self.layers.append(SoftMaxModule())
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        """
+        Performs forward pass of the input. Here an input tensor x is transformed through
+        several layer transformations.
+
+        Args:
+          x: input to the network
+        Returns:
+          out: outputs of the network
+
+        TODO:
+        Implement forward pass of the network.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        if x.ndim > 2:
+            print(x.shape)
+            x = x.reshape(x.shape[0], -1)
+        print(x.shape)
+        out = x
+        for layer in self.layers:
+            out = layer.forward(out)
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Performs backward pass given the gradients of the loss.
+
+        Args:
+          dout: gradients of the loss
+
+        TODO:
+        Implement backward pass of the network.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        for layer in reversed(self.layers):
+            dout = layer.backward(dout)
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass from any module.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Iterate over modules and call the 'clear_cache' function.
+        """
+        
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        for layer in self.layers:
+            if hasattr(layer, 'clear_cache'):
+                layer.clear_cache()
+        #######################
+        # END OF YOUR CODE    #
+        #######################
---
+++ assignment1/train_mlp_numpy.py
@@ -0,0 +1,256 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2023 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2023
+# Date Created: 2023-11-01
+################################################################################
+"""
+This module implements training and evaluation of a multi-layer perceptron in NumPy.
+You should fill in code into indicated sections.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import numpy as np
+import os
+from tqdm.auto import tqdm
+from copy import deepcopy
+from mlp_numpy import MLP
+from modules import CrossEntropyModule
+import cifar10_utils
+
+import torch
+
+
+def confusion_matrix(predictions, targets):
+    """
+    Computes the confusion matrix, i.e. the number of true positives, false positives, true negatives and false negatives.
+
+    Args:
+      predictions: 2D float array of size [batch_size, n_classes], predictions of the model (logits)
+      labels: 1D int array of size [batch_size]. Ground truth labels for
+              each sample in the batch
+    Returns:
+      confusion_matrix: confusion matrix per class, 2D float array of size [n_classes, n_classes]
+    """
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    n_classes = predictions.shape[1]
+    conf_mat = np.zeros((n_classes, n_classes))
+    pred_classes = np.argmax(predictions, axis=1)
+    for i in range(len(targets)):
+        true_label = targets[i]
+        predicted_label = pred_classes[i]
+        conf_mat[true_label, predicted_label] += 1
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+    return conf_mat
+
+
+def confusion_matrix_to_metrics(confusion_matrix, beta=1.):
+    """
+    Converts a confusion matrix to accuracy, precision, recall and f1 scores.
+    Args:
+        confusion_matrix: 2D float array of size [n_classes, n_classes], the confusion matrix to convert
+    Returns: a dictionary with the following keys:
+        accuracy: scalar float, the accuracy of the confusion matrix
+        precision: 1D float array of size [n_classes], the precision for each class
+        recall: 1D float array of size [n_classes], the recall for each clas
+        f1_beta: 1D float array of size [n_classes], the f1_beta scores for each class
+    """
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    TP = np.diag(confusion_matrix)
+    FP = np.sum(confusion_matrix, axis=0) - TP
+    FN = np.sum(confusion_matrix, axis=1) - TP
+
+    accuracy = np.sum(TP) / np.sum(confusion_matrix)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f_beta = ((1 + beta**2) * precision * recall) / (beta**2 * precision + recall)
+
+    metrics = {'accuracy': accuracy,
+               'precision': precision,
+               'recall': recall,
+               'f1_beta': f_beta}
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+    return metrics
+
+
+def evaluate_model(model, data_loader, num_classes=10):
+    """
+    Performs the evaluation of the MLP model on a given dataset.
+
+    Args:
+      model: An instance of 'MLP', the model to evaluate.
+      data_loader: The data loader of the dataset to evaluate.
+    Returns:
+        metrics: A dictionary calculated using the conversion of the confusion matrix to metrics.
+
+    TODO:
+    Implement evaluation of the MLP model on a given dataset.
+
+    Hint: make sure to return the average accuracy of the whole dataset,
+          independent of batch sizes (not all batches might be the same size).
+    """
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    conf_matrix = np.zeros((num_classes, num_classes))
+
+    for x, y in data_loader:
+        logits = model.forward(x)
+        conf_matrix += confusion_matrix(logits, y)
+
+    metrics = confusion_matrix_to_metrics(conf_matrix)
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+    return metrics
+
+
+
+def train(hidden_dims, lr, batch_size, epochs, seed, data_dir):
+    """
+    Performs a full training cycle of MLP model.
+
+    Args:
+      hidden_dims: A list of ints, specificying the hidden dimensionalities to use in the MLP.
+      lr: Learning rate of the SGD to apply.
+      batch_size: Minibatch size for the data loaders.
+      epochs: Number of training epochs to perform.
+      seed: Seed to use for reproducible results.
+      data_dir: Directory where to store/find the CIFAR10 dataset.
+    Returns:
+      model: An instance of 'MLP', the trained model that performed best on the validation set.
+      val_accuracies: A list of scalar floats, containing the accuracies of the model on the
+                      validation set per epoch (element 0 - performance after epoch 1)
+      test_accuracy: scalar float, average accuracy on the test dataset of the model that 
+                     performed best on the validation. Between 0.0 and 1.0
+      logging_info: An arbitrary object containing logging information. This is for you to 
+                    decide what to put in here.
+
+    TODO:
+    - Implement the training of the MLP model. 
+    - Evaluate your model on the whole validation set each epoch.
+    - After finishing training, evaluate your model that performed best on the validation set, 
+      on the whole test dataset.
+    - Integrate _all_ input arguments of this function in your training. You are allowed to add
+      additional input argument if you assign it a default value that represents the plain training
+      (e.g. '..., new_param=False')
+
+    Hint: you can save your best model by deepcopy-ing it.
+    """
+
+    # Set the random seeds for reproducibility
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    import ssl
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    ## Loading the dataset
+    cifar10 = cifar10_utils.get_cifar10(data_dir)
+    cifar10_loader = cifar10_utils.get_dataloader(cifar10, batch_size=batch_size,
+                                                  return_numpy=True)
+
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
+    n_inputs = 3 * 32 * 32
+    n_classes = 10
+    model = MLP(n_inputs, hidden_dims, n_classes)
+    loss_module = CrossEntropyModule()
+
+    val_accuracies = []
+    global_loss_list = []
+    best_val_accuracy = 0
+    best_model = None
+    for epoch in range(epochs):
+        print("Epoch: " + str(epoch + 1))
+        loss_list = []
+        for x, y in tqdm(cifar10_loader['train']):
+            out = model.forward(x)
+
+            loss_list.append(loss_module.forward(out, y))
+            dout = loss_module.backward(out, y)
+            model.backward(dout)
+
+            for layer in model.layers:
+                if hasattr(layer, 'grads'): 
+                    layer.params['weight'] -= lr * layer.grads['weight']
+                    layer.params['bias'] -= lr * layer.grads['bias']
+
+            model.clear_cache()
+
+        global_loss_list.append(np.mean(np.array(loss_list)))
+
+        val_metrics = evaluate_model(model, cifar10_loader['validation'], num_classes=n_classes)
+        val_accuracy = val_metrics['accuracy']
+        val_accuracies.append(val_accuracy)
+
+        if val_accuracy > best_val_accuracy:
+            best_val_accuracy = val_accuracy
+            best_model = deepcopy(model)
+
+    test_accuracy = evaluate_model(best_model, cifar10_loader['test'], num_classes=n_classes)['accuracy']
+    logging_dict = global_loss_list
+    #######################
+    # END OF YOUR CODE    #
+    #######################
+
+    return model, val_accuracies, test_accuracy, logging_dict
+
+
+if __name__ == '__main__':
+    # Command line arguments
+    parser = argparse.ArgumentParser()
+    
+    # Model hyperparameters
+    parser.add_argument('--hidden_dims', default=[128], type=int, nargs='+',
+                        help='Hidden dimensionalities to use inside the network. To specify multiple, use " " to separate them. Example: "256 128"')
+    
+    # Optimizer hyperparameters
+    parser.add_argument('--lr', default=0.1, type=float,
+                        help='Learning rate to use')
+    parser.add_argument('--batch_size', default=128, type=int,
+                        help='Minibatch size')
+
+    # Other hyperparameters
+    parser.add_argument('--epochs', default=10, type=int,
+                        help='Max number of epochs')
+    parser.add_argument('--seed', default=42, type=int,
+                        help='Seed to use for reproducing results')
+    parser.add_argument('--data_dir', default='data/', type=str,
+                        help='Data directory where to store/find the CIFAR10 dataset.')
+
+    args = parser.parse_args()
+    kwargs = vars(args)
+
+    model, val_accuracies, test_accuracy, logging_dict = train(**kwargs)
+    # Feel free to add any additional functions, such as plotting of the loss curve here
+    import matplotlib.pyplot as plt
+    print(val_accuracies)
+    print(test_accuracy)
+    plt.plot(range(1,len(logging_dict)+1), logging_dict)
+    plt.title("Average loss per epoch")
+    plt.xlabel("Epoch")
+    plt.ylabel("Average loss")
+    plt.show()
